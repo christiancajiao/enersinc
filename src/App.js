7a26c42d8f300b2f5e8877167842567ca286c0d8
christiancajiao <christiancamilocajiaochacon@gmail.com>
2022-09-02 12:11:25 -0500

    instalation redux antd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
+import { Table } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
+  const [data, setData] = useState([])
+
+  const url = 'https://gorest.co.in/public/v2/users'
+  useEffect(() => {
+    console.log(data)
+    fetch(url).then(response => response.json()).then(data => setData(data))
+  }, [])
+
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Gender',
+      dataIndex: 'gender',
+    },
+    {
+      title: 'email',
+      dataIndex: 'email',
+    },
+    {
+      title: 'status',
+      dataIndex: 'status',
+    },
+    {
+      title: 'Add',
+      render: (text, record) => (
+        <button onClick={()=> console.log(record)}>
+          {"Add"}
+        </button>
+       ),
+    }
+  ];
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} dataSource={data} size="big" pagination={false} />
     </div>
   );
 }
